4f5f87d57f9731508d0c26e92afe350ef56087b1
Emmanuel Lopez <eelopez29@gmail.com>
2022-02-21 23:37:18 -0300

    Modificando desafio clase 11, chat con websockets

---
+++ scripts/api.js
@@ -0,0 +1,81 @@
+class Api {
+
+  constructor() {
+    this.productos = [
+      {
+        title:"Lovaglio Malbec",
+        price:1500,
+        thumbnail:"/images/L-Malbec.jpg"
+      },
+    ];
+  }
+
+
+  getAll() {
+      return this.productos;
+  }
+
+  getProductoById(id) {
+
+      const resultado = this.productos.find(idBuscado => idBuscado.id === parseInt(id));
+      if (resultado === undefined) {
+          return { error: 'producto no encontrado' };
+      } else {
+          return resultado;
+      }
+  }
+
+  setProducto(producto) {
+
+      const id = this.productos.length + 1;
+
+      if (producto.title && producto.price && producto.thumbnail) {
+          this.productos.push(Object.defineProperty(producto, 'id', {
+              value: id,
+              writable: true,
+              configurable: true,
+              enumerable: true
+          }));
+
+          return this.productos[id - 1];
+      } else {
+          return 'Ingresaste un formato incorrecto de producto';
+      }
+  }
+
+  updateProductoById(id, producto) {
+      const resultado = this.productos.find(idBuscado => idBuscado.id === parseInt(id));
+
+      if (resultado === undefined) {
+          return { error: 'producto no encontrado' };
+      } else {
+          if (producto.title && producto.price && producto.thumbnail) {
+              Object.defineProperties(this.productos.find(idBuscado => idBuscado.id === parseInt(id)), {
+                  'title': {
+                      value: producto.title
+                  },
+                  'price': {
+                      value: producto.price
+                  },
+                  'thumbnail': {
+                      value: producto.thumbnail
+                  }
+              })
+          } else {
+              return 'No es el formato de producto que podes ingresar';
+          }
+      }
+  }
+
+  deleteProductoById(id) {
+      const resultado = this.productos.find(idBuscado => idBuscado.id === parseInt(id));
+
+      if (resultado === undefined) {
+          return { error: 'producto no encontrado' };
+      } else {
+          this.productos = this.productos.filter(idEliminado => idEliminado.id !== parseInt(id));
+      }
+  }
+}
+
+module.exports = {Api}; 
